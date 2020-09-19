a62614710a756a06e860295b9851c0156d30cee0
VoDuyNghia <nghia97dn@gmail.com>
2020-09-19 22:08:52 +0700

    Update3-19-09-2020

--- resources/views/ui/common/plugin.blade.php
+++ resources/views/ui/common/plugin.blade.php
@@ -2,6 +2,6 @@
     <img class="back-to-top" src="{{ asset('images/top.png') }}" alt="Quang Vinh - Coffee">
 </a>
 
-<a id="call-phone" href="tel:+898085010">
+<a id="call-phone" href="tel:(+84)898987567">
     <img class="call-phone" src="{{ asset('images/phone.png') }}" alt="Quang Vinh - Coffee">
 </a>
